ece0cf0d45df15ce48816e7ed9e0e7ab6e4d7a20
anthonygalgano <anthonygalgano@localhost>
2020-10-29 15:52:41 -0400

    Wrote a test case for tile

--- Individual_Project_Anthony_Galgano/test/model/TestTile.java
+++ Individual_Project_Anthony_Galgano/test/model/TestTile.java
@@ -4,11 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import puzzle.model.Tile;
+
 class TestTile {
 
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConstruction() {
+		Tile tile = new Tile(1, 2, 10, true);
+		assertEquals (1, tile.width);
+		assertEquals (2, tile.height);
+		assertEquals (10, tile.getValue());
+		assertTrue(tile.getInPlay());
 	}
 
 }
