c2de6fde407d8bb9d5b33d14af6165292c02fed2
agalgano08 <agalgano@wpi.edu>
2020-11-02 13:59:11 -0500

    tested reset

--- Individual_Project_Anthony_Galgano/test/controller/TestResetController.java
+++ Individual_Project_Anthony_Galgano/test/controller/TestResetController.java
@@ -1,14 +1,32 @@
 package controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class TestResetController {
+import puzzle.controller.ResetController;
+import puzzle.model.Coordinate;
+import puzzle.model.MoveType;
+import puzzle.model.Tile;
+
+
+
+class TestResetController extends AppTestCase{
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testReset() {
+		Tile t = this.getTile(new Coordinate(1,1)).get(); 
+		model.setSelectedTile(t);
+		assertTrue (model.tryMove(MoveType.Left)); 	// Move middle tile left
+		assertTrue (getTile(new Coordinate(0,1)).get().getInPlay());
+		assertFalse (t.getInPlay());
+		ResetController rtc = new ResetController(model, app);
+		rtc.reset();
+		
+		assertTrue(getTile(new Coordinate(1,1)).get().getInPlay());
 	}
 
 }
